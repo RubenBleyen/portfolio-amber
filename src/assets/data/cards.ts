061d692f8687c4827dc4228fbc7bf76da803ea09
RubenBleyen <ruben.bleyen@hotmail.com>
2023-05-05 19:14:31 +0200

    To webp images

--- src/assets/data/cards.ts
+++ src/assets/data/cards.ts
@@ -2,41 +2,41 @@ export default [
     {
       "id": 4,
       "name": "Collage, model: Solenne Devos",
-      "image_background": "200821_ALTR_BookPostInstagram_AmberJongsma kopie.jpg",
+      "image_background": "200821_ALTR_BookPostInstagram_AmberJongsma kopie.webp",
     },
     {
       "id": 51,
       "name": "Travelbook: Emergency passport",
-      "image_background": "DIOR_MENS.jpeg",   
+      "image_background": "DIOR_MENS_SPRING_2021_SKI_CAPSULE-©-BRETT_LLOYD_11.webp",   
     },
     {
         "id": 4,
         "name": "Fake news",
-        "image_background": "IMG_0559.PNG",
+        "image_background": "IMG_0559.webp",
       },    
       {
         "id": 4,
         "name": "Collage, model: Solenne Devos",
-        "image_background": "IMG_0086 2.jpg",
+        "image_background": "IMG_0086 2.webp",
       },
       {
           "id": 4,
           "name": "Fake news",
-          "image_background": "Screenshot 2023-05-01 at 21.50.24.png",
+          "image_background": "Screenshot 2023-05-01 at 21.50.24.webp",
         },
         {
           "id": 4,
           "name": "Collage, model: Solenne Devos",
-          "image_background": "Screenshot 2023-05-01 at 21.55.03.png",
+          "image_background": "Screenshot 2023-05-01 at 21.55.03.webp",
         },
         {
           "id": 51,
           "name": "Travelbook: Emergency passport",
-          "image_background": "Screenshot 2023-05-01 at 22.26.21.png",   
+          "image_background": "Screenshot 2023-05-01 at 22.26.21.webp",   
         },
         {
             "id": 4,
             "name": "Fake news",
-            "image_background": "Screenshot 2023-05-01 at 22.27.10.png",
+            "image_background": "Screenshot 2023-05-01 at 22.27.10.webp",
           },         
 ]
