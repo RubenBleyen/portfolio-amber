5d966dc5f99f45362da48b889b757a8e46a2ae2b
RubenBleyen <ruben.bleyen@hotmail.com>
2023-05-05 19:22:30 +0200

    no height adapting

--- src/components/SlideShow.tsx
+++ src/components/SlideShow.tsx
@@ -6,7 +6,7 @@ const SlideShow = () => {
   const {data, isLoading, error} = useCards()
   return (
     <Box display="relative" boxSize={600}>
-  <Carousel wrapAround={true} speed={3000} animation="fade" pauseOnHover={true} autoplay={true} autoplayInterval={5000} adaptiveHeight={true}>
+  <Carousel wrapAround={true} speed={3000} animation="fade" pauseOnHover={true} autoplay={true} autoplayInterval={5000}>
   {data.map((portfolio) => (
         <img key={portfolio.id} src={portfolio.image_background}></img>
     ))}
