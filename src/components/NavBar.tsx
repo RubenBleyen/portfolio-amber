5f56aa5aa17f2e0bbfc2ece5499f8e55b8dd797e
RubenBleyen <ruben.bleyen@hotmail.com>
2023-05-05 17:00:13 +0200

    Added logo and overview

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,35 +10,32 @@ import {
   IconButton,
   useBreakpointValue,
 } from "@chakra-ui/react";
-import logo from "../assets/logo.webp";
-import { FiMenu } from 'react-icons/fi'
+import logo from "../assets/2022_AmberJongsma.png";
+import { FiMenu } from "react-icons/fi";
 
 const NavBar = () => {
-    const isDesktop = useBreakpointValue({ base: false, lg: true })
-    return (
-        <Box as="nav" padding={7} bg="bg-surface" boxShadow="md">
-          <Center>
-            <HStack spacing="10" justify="space-between">
-            <Image src={logo} boxSize='60px'></Image>
-            <Heading>Amber Jongsma</Heading>
-              {isDesktop ? (
-                <Flex float={'right'} justify="space-between" flex="1">
-                  <ButtonGroup variant="link" spacing="8">
-                    {['Home', 'Portfolio', 'About', 'Contact'].map((item) => (
-                      <Button key={item}>{item}</Button>
-                    ))}
-                  </ButtonGroup>
-                </Flex>
-              ) : (
-                <IconButton
-                  variant="ghost"
-                  icon={<FiMenu fontSize="1.25rem" />}
-                  aria-label="Open Menu"
-                />
-              )}
-            </HStack>
-          </Center>
-        </Box>
+  const isDesktop = useBreakpointValue({ base: false, lg: true });
+  return (
+    <Box as="nav" padding={7} bg="bg-surface" boxShadow="md">
+        <Flex justify="space-between" marginLeft={10}  marginRight={10}>
+          <Image objectFit="scale-down" src={logo} width="250px" ></Image>
+          {isDesktop ? (
+            <Flex>
+              <ButtonGroup float="right" variant="link" spacing="8">
+                {["OVERVIEW"].map((item) => (
+                  <Button key={item}>{item}</Button>
+                ))}
+              </ButtonGroup>
+            </Flex>
+          ) : (
+            <IconButton
+              variant="ghost"
+              icon={<FiMenu fontSize="1.25rem" />}
+              aria-label="Open Menu"
+            />
+          )}
+        </Flex>
+    </Box>
   );
 };
 export default NavBar;
